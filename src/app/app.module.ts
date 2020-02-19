f333a2049fa83d4d8f6a7f04ea6116a3d265ff8a
smohammadfy <m.faniyazdi@gmail.com>
2020-02-19 17:31:31 +0330

    table for page edited!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,49 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  MatButtonModule, MatButtonToggleModule,
+  MatCardModule, MatDatepickerModule,
+  MatFormFieldModule,
+  MatGridListModule, MatIconModule,
+  MatInputModule,
+  MatListModule, MatSidenavModule, MatTableModule,
+  MatToolbarModule, MatNativeDateModule
+} from '@angular/material';
+import {FormsModule} from '@angular/forms';
+import { LastsellmoreComponent } from './lastsellmore/lastsellmore.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LastsellmoreComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatListModule,
+    MatGridListModule,
+    MatIconModule,
+    MatTableModule,
+    MatSidenavModule,
+    MatButtonToggleModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
